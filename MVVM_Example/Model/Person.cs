73c88b2314b74a019e3a002d2f9fbbe613b65033
nati <natibir07@gmail.com>
2019-05-30 00:37:36 +0300

    Init commit to push

---
+++ MVVM_Example/Model/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_Example.Model
+{
+    public class Person
+    {
+        private string fname;
+
+        public string Fname
+        {
+            get { return fname; }
+            set
+            {
+                fname = value;
+            }
+        }
+
+        private string lname;
+
+        public string Lname
+        {
+            get { return lname; }
+            set
+            {
+                lname = value;
+            }
+        }
+
+        private string fullname;
+
+        public string Fullname
+        {
+            get { return fullname = fname + " " + lname; }
+            set
+            {
+                if (fullname != value)
+                {
+                    fullname = value;
+
+                }
+            }
+        }
+
+    }
+}
---
+++ MVVM_Example/ViewModel/PersonViewModel.cs
@@ -0,0 +1,79 @@
+﻿using MVVM_Example.Model;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using MVVM_Example.Command;
+using System.Windows.Input;
+
+namespace MVVM_Example.ViewModel
+{
+
+    public class PersonViewModel : INotifyPropertyChanged
+    {
+        public PersonViewModel()
+        {
+            Person = new Person();
+            Persons = new ObservableCollection<Person>();
+        }
+
+        private Person _person;
+        public Person Person
+        {
+            get { return _person; }
+            set { _person = value; OnPropertyChanged("Person"); }
+        }
+
+        private ObservableCollection<Person> _persons;
+        public ObservableCollection<Person> Persons
+        {
+            get { return _persons; }
+            set { _persons = value; OnPropertyChanged("Persons"); }
+        }
+
+        private ICommand _submitCommand;
+        public ICommand SubmitCommand
+        {
+            get
+            {
+                if (_submitCommand == null)
+                {
+                    _submitCommand = new RelayCommand(SubmitExecute, CanSubmitExecute, false);
+                    return _submitCommand;
+                }
+                else
+                {
+                    return _submitCommand;
+                }
+            }
+        }
+        private void SubmitExecute(object parameter)
+        {
+            Persons.Add(Person);
+        }
+
+        private bool CanSubmitExecute(object parameter)
+        {
+            if (String.IsNullOrEmpty(Person.Fname) || String.IsNullOrEmpty(Person.Lname))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propname)
+        {
+            PropertyChangedEventHandler ph = PropertyChanged;
+
+            if (ph != null)
+            {
+                ph(this, new PropertyChangedEventArgs(propname));
+            }
+
+        }
+    }
+}
